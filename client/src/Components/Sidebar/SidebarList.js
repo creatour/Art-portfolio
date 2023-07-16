37cf53e5cd9219e962abb04c141f51c4456022bc
creatour <artmiracle65@gmail.com>
2023-07-16 08:48:24 +0800

    Create SidebarList.js

---
+++ client/src/Components/Sidebar/SidebarList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./SidebarList.css"; 
+import profilepic from "../../Image/art.jpg";
+
+const SidebarList = ({expandSidebar}) => {
+    return (
+        <React.Fragment>
+         {expandSidebar ? (
+         <div className="navbar-items">
+            <div className='sidebar-profile-pic'>
+                <img src={profilepic} alt='profile picture'/>
+
+             </div>
+
+            </div>
+
+        ) : (
+          <div></div>  
+        )}
+        </React.Fragment>
+
+    );
+};
+
+export default SidebarList
